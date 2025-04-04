e3be55827f636290757d56c135f460df3b626c04
levisrabah <levisrabah@gmail.com>
2025-04-04 19:58:31 +0300

    Add VSCode settings and update image sources; correct software title in script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Fronted Developer","FullStack Developer", "Website Designer & Developer", "Mobile App. Developer" ],
+    strings: ["Fronted Developer","FullStack Sofware Developer", "Website Designer & Developer", "Mobile App. Developer" ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
